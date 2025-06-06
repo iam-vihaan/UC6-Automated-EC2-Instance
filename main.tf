3b9813195fa25aac243c47a95ded6765f802e468
iam-vihaan <asathyavihaan@gmail.com>
2025-06-06 12:24:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ terraform {
   required_version = ">= 1.10.0"
 }
 
-module "ec2_scheduler" {
-  source        = "./modules/ec2-scheduler"
+module "ec2" {
+  source        = "./modules/ec2"
   instance_id   = var.instance_id
   start_schedule = var.start_schedule # 8 AM weekdays
   stop_schedule  = var.stop_schedule # 5 PM weekdays
