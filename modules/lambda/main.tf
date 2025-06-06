2a8aba0a1cc13f6394b6ca92b21d64d4e436433b
iam-vihaan <asathyavihaan@gmail.com>
2025-06-06 14:47:30 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,3 +1,8 @@
+data "archive_file" "zip_file" {
+  type        = "zip"
+  source_file = var.source_file
+  output_path = "${path.module}/${var.function_name}.zip"
+}
 
 resource "aws_lambda_function" "lamb_fn" {
   filename      = data.archive_file.zip_file.output_path
